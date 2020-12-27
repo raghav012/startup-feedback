3c4125460f1cb4bc444b50b64643fb75b4d5ce12
raghav012 <43701598+raghav012@users.noreply.github.com>
2020-12-27 23:25:42 +0530

    auth part done

--- index.js
+++ index.js
@@ -1,10 +1,31 @@
-const express=require("express");
+const express = require("express");
+const mongoose=require('mongoose');
+const keys=require('./config/keys');
+const cookieSession=require('cookie-session');
+const Passport=require('passport');
+const passport = require("passport");
 
-const app =express();
 
-app.get('/',(req,res)=>{
-    res.send("hello i am here");
-});
+require('./models/User');
+require('./services/passport');
 
-const PORT= process.env.PORT || 3000;
+mongoose.connect(keys.mongoURI);
+
+
+const app = express();
+
+app.use(
+    cookieSession({
+        maxAge:30*24*60*60*1000,
+        keys:[keys.cookieKey],
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT);
